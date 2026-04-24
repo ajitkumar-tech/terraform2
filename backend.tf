70f574fac2782ea8aa07eaeb8b15e67ea93bcef5
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-24 08:01:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-eks-pvc-demo"
-    key    = "pvcv/terraform.tfstate" 
+    bucket = "eks-cluster-demo1"
+    key    = "eks-cluster-demo1/terraform.tfstate" 
     region = "ap-south-1"
   }
 }
